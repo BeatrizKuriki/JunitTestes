886f62d3eab46a3b0748f4f8063ba1bf27b9b3c8
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-18 23:14:32 -0300

    primeiro teste unitário para verificar a lógica de depósito

---
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -0,0 +1,32 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+	/*
+	 * PARA FAZER TESTES VOCÊ DEVE UTILIZAR O PADRÃO AAA
+	 * A - ARRANGE: INSTANCIE OS OBJETOS NECESSÁRIOS;
+	 * A - ACT: EXECUTE AS AÇÕES NECESSÁRIAS;
+	 * A - ASSERT: DECLARE O QUE DEVERIA ACONTECER (RESULTADO ESPERADO)
+	 *  
+	 * <AÇÃO> should <EFEITO> [when <CENÁRIO>]
+	 */
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		//feito o arrange
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account (1L, 0.0);
+		
+		//feito o act- execute as ações necessárias
+		acc.deposit(amount);
+		
+		//assert - declarar o que deverá acontecer- no caso o saldo da conta deve ter 2% a menos como resultado
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+}
